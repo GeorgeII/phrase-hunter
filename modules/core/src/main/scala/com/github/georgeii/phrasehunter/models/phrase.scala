411f230358fa40a9eece1ea46aa8cbff09bcd405
GeorgeII <egorzorikov@gmail.com>
2021-11-30 09:49:27 +0400

    Refined Phrase type redefined to work correctly

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/models/phrase.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/models/phrase.scala
@@ -1,12 +1,11 @@
 package com.github.georgeii.phrasehunter.models
 
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.types.string.{ FiniteString, NonEmptyString }
+import eu.timepit.refined.types.string.NonEmptyFiniteString
 import io.estatico.newtype.macros.newtype
 
 object phrase {
 
-  type PhraseRefined = NonEmptyString Refined FiniteString[100]
+  type PhraseRefined = NonEmptyFiniteString[100]
 
   @newtype
   case class Phrase(value: PhraseRefined)
