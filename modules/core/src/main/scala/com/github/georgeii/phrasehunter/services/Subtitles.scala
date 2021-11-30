ebcb3a1b72d096e0d3cb6e015543f8072e3e5633
GeorgeII <egorzorikov@gmail.com>
2021-12-01 01:00:27 +0400

    Subtitle service refactored + now takes an IO-effect to encapsulate its mapping inside

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/services/Subtitles.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/services/Subtitles.scala
@@ -1,13 +1,14 @@
 package com.github.georgeii.phrasehunter.services
 
 import cats.Applicative
-import cats.effect.{ Resource, Sync }
+import cats.effect.Sync
 import cats.implicits._
 import com.github.georgeii.phrasehunter.models.phrase._
-import com.github.georgeii.phrasehunter.models.{ Subtitle, SubtitleOccurrenceDetails }
+import com.github.georgeii.phrasehunter.models.SubtitleOccurrenceDetails
+import com.github.georgeii.phrasehunter.programs.util.FileReader
+import com.github.georgeii.phrasehunter.programs.PhraseSearcher
 
 import java.io.File
-import scala.io.{ BufferedSource, Source }
 
 trait Subtitles[F[_]] {
   def findAll(phrase: Phrase): F[List[SubtitleOccurrenceDetails]]
@@ -15,22 +16,25 @@ trait Subtitles[F[_]] {
 
 object Subtitles {
   def make[F[_]: Sync](
-      subtitleStorage: List[File]
+      subtitleStorage: F[List[File]]
   ): Subtitles[F] = {
     new Subtitles[F] {
       override def findAll(phrase: Phrase): F[List[SubtitleOccurrenceDetails]] = {
-        subtitleStorage
-          .map { file =>
-            val fileResource = makeReadFileResource[F](file)
+        subtitleStorage.map { filesList =>
+          filesList
+            .map { file =>
+              val fileResource        = FileReader.makeFileResource[F](file)
+              val filenameNoExtension = file.getName.split('.').toList.dropRight(1).mkString
 
-            fileResource.use { bufferedSource =>
-              Applicative[F].pure(
-                findOccurrencesInParticularFile(phrase, bufferedSource.mkString, file.getAbsolutePath)
-              )
+              fileResource.use { bufferedSource =>
+                Applicative[F].pure(
+                  PhraseSearcher.findOccurrencesInFile(phrase, bufferedSource.mkString, filenameNoExtension)
+                )
+              }
             }
-          }
-          .sequence
-          .map(_.flatten)
+            .sequence
+            .map(_.flatten)
+        }.flatten
       }
     }
   }
@@ -45,64 +49,4 @@ object Subtitles {
 //    } yield files.flatten
 //  }
 
-  def getAllSubtitleFilesInDirectory[F[_]: Applicative](directory: String): F[List[File]] = {
-    val filesDirectory = new File(directory)
-
-    if (filesDirectory.exists && filesDirectory.isDirectory) {
-      filesDirectory.listFiles.filter(_.isFile).toList.pure[F]
-    } else {
-      List.empty[File].pure[F]
-    }
-  }
-
-  private def makeReadFileResource[F[_]: Sync](
-      file: File
-  ): Resource[F, BufferedSource] = {
-    Resource.fromAutoCloseable {
-      Applicative[F].pure(Source.fromFile(file))
-    }
-  }
-
-  private def findOccurrencesInParticularFile(
-      phrase: Phrase,
-      source: String,
-      fileAbsPath: String
-  ): List[SubtitleOccurrenceDetails] = {
-    val splitSourceBySeparateSubtitle = source.split("\r\n\r\n").toList
-
-    val parsedSubtitles = for {
-      separateSubtitleInFile <- splitSourceBySeparateSubtitle
-      subtitleParts = separateSubtitleInFile.split("\r\n")
-    } yield Subtitle(subtitleParts(0).toInt, subtitleParts(1), subtitleParts.drop(2).mkString(" ").toLowerCase)
-
-    val subtitlesThatContainPhrase = parsedSubtitles.filter(_.text.contains(phrase.toString))
-
-    subtitlesThatContainPhrase.map { subtitle =>
-      val (startString, endString) = extractTimestampsFromSubtitleString(subtitle.timestamp)
-      val startMillis              = convertStringTimeToMillis(startString)
-      val endMillis                = convertStringTimeToMillis(endString)
-
-      SubtitleOccurrenceDetails(fileAbsPath, subtitle.number, startMillis, endMillis, subtitle.text)
-    }
-  }
-
-  private def extractTimestampsFromSubtitleString(timeString: String): (String, String) = {
-    // 00:02:19,482 --> 00:02:21,609
-    val startAndEndTimestamps = timeString.split(" --> ")
-
-    (startAndEndTimestamps(0), startAndEndTimestamps(1))
-  }
-
-  private def convertStringTimeToMillis(timestamp: String): Long = {
-    // 00:02:19,482
-    val hoursMinutesSecondsSplit = timestamp.split(":")
-    val secondsMillisSplit       = hoursMinutesSecondsSplit(2).split(",")
-
-    val hours   = hoursMinutesSecondsSplit(0).toLong
-    val minutes = hoursMinutesSecondsSplit(1).toLong
-    val seconds = secondsMillisSplit(0).toLong
-    val millis  = secondsMillisSplit(1).toLong
-
-    hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000 + millis
-  }
 }
