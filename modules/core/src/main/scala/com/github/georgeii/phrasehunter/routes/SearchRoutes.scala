b264cf98eaa2880c2287114b01e49af126def005
GeorgeII <egorzorikov@gmail.com>
2021-12-02 00:58:28 +0400

    First endpoint added

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/routes/SearchRoutes.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/routes/SearchRoutes.scala
@@ -1,13 +1,19 @@
 package com.github.georgeii.phrasehunter.routes
 
-import cats.Monad
 import com.github.georgeii.phrasehunter.models.phrase._
+import com.github.georgeii.phrasehunter.codecs.phrase._
+import com.github.georgeii.phrasehunter.codecs.subtitleOccurrenceDetails._
 import com.github.georgeii.phrasehunter.services.Subtitles
+
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import cats.effect.kernel.Concurrent
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
+import org.http4s.circe.CirceEntityCodec._
 
-final case class SearchRoutes[F[_]: Monad](
+final case class SearchRoutes[F[_]: Concurrent](
     subtitles: Subtitles[F]
 ) extends Http4sDsl[F] {
 
@@ -17,6 +23,13 @@ final case class SearchRoutes[F[_]: Monad](
     case GET -> Root =>
       Ok("I'm fine!")
 //      Ok(subtitles.findAll(Phrase("smth")))
+
+    case req @ POST -> Root =>
+      for {
+        phrase         <- req.as[Phrase]
+        foundSubtitles <- subtitles.findAll(phrase)
+        resp           <- Ok(foundSubtitles)
+      } yield resp
   }
 
   val routes: HttpRoutes[F] = Router(
