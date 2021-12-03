c6b7c4af5c11f5d1fd6edab853c798ebc7715886
GeorgeII <egorzorikov@gmail.com>
2021-12-03 10:40:06 +0400

    MediaPlayer resource making added

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/programs/util/FileReader.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/programs/util/FileReader.scala
@@ -4,18 +4,13 @@ import cats.Applicative
 import cats.effect.{ Resource, Sync }
 import cats.implicits._
 
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory
+import uk.co.caprica.vlcj.player.base.MediaPlayer
+
 import java.io.File
 import scala.io.{ BufferedSource, Source }
 
 object FileReader {
-
-  def makeFileResource[F[_]: Sync](
-      file: File
-  ): Resource[F, BufferedSource] = {
-    Resource.fromAutoCloseable {
-      Applicative[F].pure(Source.fromFile(file))
-    }
-  }
 
   def getAllFilesInDirectory[F[_]: Applicative](directory: String): F[List[File]] = {
     val filesDirectory = new File(directory)
@@ -26,5 +21,33 @@ object FileReader {
       List.empty[File].pure[F]
     }
   }
+
+  def getNameNoExtension(file: File): String = {
+    file.getName.split('.').toList.dropRight(1).mkString
+  }
+
+  def makeSubtitleFileResource[F[_]: Sync](file: File): Resource[F, BufferedSource] = {
+    Resource.fromAutoCloseable {
+      Applicative[F].pure(Source.fromFile(file))
+    }
+  }
+
+  /**
+    * @param mrl Media Resource Locator. The simplest example is "/user/username/videos/filename.mkv"
+    */
+  def makeMediaFileResource[F[_]: Applicative](mrl: String): Resource[F, MediaPlayer] =
+    Resource.make {
+      Applicative[F].pure {
+        // the content of variables mutates
+        val mediaPlayerFactory = new MediaPlayerFactory
+        val mediaPlayer        = mediaPlayerFactory.mediaPlayers.newMediaPlayer()
+
+        mediaPlayer.media().prepare(mrl)
+
+        mediaPlayer
+      }
+    } { mediaPlayer =>
+      Applicative[F].pure(mediaPlayer.release())
+    }
 
 }
