9c3c5e60540a90d11b7de61ffa29b62d58dd7c40
GeorgeII <egorzorikov@gmail.com>
2021-12-10 19:54:07 +0400

    Doobie transactor initialization added

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/PhraseHunterServer.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/PhraseHunterServer.scala
@@ -8,22 +8,30 @@ import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import org.http4s.server.middleware.Logger
-
 import com.github.georgeii.phrasehunter.routes.SearchRoutes
 import com.github.georgeii.phrasehunter.services.Subtitles
 import com.github.georgeii.phrasehunter.util.FileReader
+import doobie.Transactor
+import doobie.util.transactor.Transactor.Aux
 
 object PhraseHunterServer {
 
   def stream[F[_]: Async]: Stream[F, Nothing] = {
+    val subtitleDirectory = "data/subtitles/"
+    val subtitleFiles     = FileReader.getAllFilesInDirectory(subtitleDirectory)
+
+    val postgresTransactor: Aux[F, Unit] = Transactor.fromDriverManager[F](
+      "org.postgresql.Driver",         // driver classname
+      "jdbc:postgresql:phrase-hunter", // connect URL (driver-specific)
+      "postgres",                      // user
+      "password"                       // password
+    )
+
     for {
       client <- Stream.resource(EmberClientBuilder.default[F].build)
 
-      subtitleDirectory = "data/subtitles/"
-      subtitleFiles     = FileReader.getAllFilesInDirectory(subtitleDirectory)
-
       httpApp = (
-        SearchRoutes(Subtitles.make(subtitleFiles)).routes
+        SearchRoutes(Subtitles.make(postgresTransactor, subtitleFiles)).routes
       ).orNotFound
 
       // With Middlewares in place
@@ -36,7 +44,7 @@ object PhraseHunterServer {
           .withPort(port"8080")
           .withHttpApp(finalHttpApp)
           .build >>
-          Resource.eval(Async[F].never)
+            Resource.eval(Async[F].never)
       )
     } yield exitCode
   }.drain
