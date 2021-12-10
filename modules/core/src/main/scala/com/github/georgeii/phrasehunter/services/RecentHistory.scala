ac51f41b57f14480df3420c98786a986a4cf4a31
GeorgeII <egorzorikov@gmail.com>
2021-12-10 17:26:19 +0400

    Creation of the requests table

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/services/RecentHistory.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/services/RecentHistory.scala
@@ -4,12 +4,12 @@ import cats.effect.{ Resource, Sync }
 import doobie.implicits._
 import doobie.util.transactor.Transactor.Aux
 
-import com.github.georgeii.phrasehunter.models.phrase.Phrase
+import com.github.georgeii.phrasehunter.models.PhraseDatabaseRecord
 
 trait RecentHistory[F[_]] {
-  def getAllRecent(n: Int): F[List[Phrase]]
-  def getRecentFound(n: Int): F[List[Phrase]]
-  def getRecentNotFound(n: Int): F[List[Phrase]]
+  def getAllRecent(n: Int): F[List[PhraseDatabaseRecord]]
+  def getRecentFound(n: Int): F[List[PhraseDatabaseRecord]]
+  def getRecentNotFound(n: Int): F[List[PhraseDatabaseRecord]]
 }
 
 object RecentHistory {
@@ -18,38 +18,38 @@ object RecentHistory {
       redis: Resource[F, _]
   ): RecentHistory[F] =
     new RecentHistory[F] {
-      override def getAllRecent(n: Int = 10): F[List[Phrase]] =
+      override def getAllRecent(n: Int = 10): F[List[PhraseDatabaseRecord]] =
         sql"""
             select id, phrase, matches_number, timestamp from search_history
             order by id desc
         """
-          .query[Phrase]
+          .query[PhraseDatabaseRecord]
           .stream
           .take(n)
           .compile
           .toList
           .transact(postgresXa)
 
-      override def getRecentFound(n: Int = 5): F[List[Phrase]] =
+      override def getRecentFound(n: Int = 5): F[List[PhraseDatabaseRecord]] =
         sql"""
             select id, phrase, matches_number, timestamp from search_history
             where matches_number > 0
             order by id desc
         """
-          .query[Phrase]
+          .query[PhraseDatabaseRecord]
           .stream
           .take(n)
           .compile
           .toList
           .transact(postgresXa)
 
-      override def getRecentNotFound(n: Int = 5): F[List[Phrase]] =
+      override def getRecentNotFound(n: Int = 5): F[List[PhraseDatabaseRecord]] =
         sql"""
             select id, phrase, matches_number, timestamp from search_history
             where matches_number = 0
             order by id desc
         """
-          .query[Phrase]
+          .query[PhraseDatabaseRecord]
           .stream
           .take(n)
           .compile
