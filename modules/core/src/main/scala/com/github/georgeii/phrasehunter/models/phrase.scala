8971b3c2acdc882126856aea8536238fb2fffd28
GeorgeII <egorzorikov@gmail.com>
2021-12-11 16:01:32 +0400

    Phrase length is 200 now

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/models/phrase.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/models/phrase.scala
@@ -5,7 +5,7 @@ import io.estatico.newtype.macros.newtype
 
 object phrase {
 
-  type PhraseRefined = NonEmptyFiniteString[100]
+  type PhraseRefined = NonEmptyFiniteString[200]
 
   @newtype
   case class Phrase(value: PhraseRefined)
