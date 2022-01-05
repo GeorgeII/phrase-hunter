bb27f7701e0634cb932ff01076bc3c372df0d666
GeorgeII <egorzorikov@gmail.com>
2022-01-05 22:06:25 +0400

    Searching algo moved to FileReader class

--- modules/core/src/main/scala/com/github/georgeii/phrasehunter/util/FileReader.scala
+++ modules/core/src/main/scala/com/github/georgeii/phrasehunter/util/FileReader.scala
@@ -2,9 +2,12 @@ package com.github.georgeii.phrasehunter.util
 
 import cats.Applicative
 import cats.effect.{ Resource, Sync }
+import com.github.georgeii.phrasehunter.models.phrase.Phrase
+import com.github.georgeii.phrasehunter.models.{ Subtitle, SubtitleOccurrenceDetails }
 
 import java.io.File
 import scala.io.{ BufferedSource, Source }
+import scala.util.Try
 
 object FileReader {
 
@@ -22,5 +25,35 @@ object FileReader {
 
   def makeSubtitleFileResource[F[_]: Sync](file: File): Resource[F, BufferedSource] =
     Resource.fromAutoCloseable { Applicative[F].pure(Source.fromFile(file)) }
+
+  def extractInfoFromSubtitle(subtitle: String): Either[Throwable, Subtitle] = {
+    val metaInfo = subtitle.split("\r\n")
+
+    Try {
+      Subtitle(metaInfo(0).toInt, metaInfo(1), metaInfo.drop(2).mkString(" "))
+    }.toEither
+  }
+
+  def findOccurrencesInFile(
+      phrase: Phrase,
+      text: String,
+      filename: String
+  ): List[SubtitleOccurrenceDetails] = {
+    val separateSubtitles = text.split("\r\n\r\n")
+
+    val subtitlesThatContainPhrase = separateSubtitles.view
+      .map(sub => extractInfoFromSubtitle(sub))
+      .filter(_.isRight)
+      .map(_.toOption.get)
+      .filter(_.text.toLowerCase.contains(phrase.toString.toLowerCase))
+      .map { sub =>
+        val (startMillis, endMillis) = TimeConverter.extractStartEndTimestamps(sub.timestamp)
+
+        SubtitleOccurrenceDetails(filename, sub.number, startMillis, endMillis, sub.text)
+      }
+      .toList
+
+    subtitlesThatContainPhrase
+  }
 
 }
